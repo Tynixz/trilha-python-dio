cc4a4a731c9a1dd07b7afc6d212971bea0e1c99a
Tayná R. Moreira <taynamore11@gmail.com>
2026-02-02 19:51:23 -0300

    Atualizacao em funçoes

--- 01 - Estrutura de dados/05 - Funções/03_args_kwargs.py
+++ 01 - Estrutura de dados/05 - Funções/03_args_kwargs.py
@@ -6,6 +6,7 @@ def exibir_poema(data_extenso, *args, **kwargs):
 
 
 exibir_poema(
+    "Segunda-feira, 02 de Fevereiro de 2026",
     "Zen of Python",
     "Beautiful is better than ugly.",
     "Explicit is better than implicit.",
