39f680e03d1c05f1555a12b684f34407e6fad271
Tayná R. Moreira <taynamore11@gmail.com>
2026-02-03 17:13:24 -0300

    Alteraçoes dos testes do estudo

--- 01 - Estrutura de dados/05 - Funções/07_escopo_local_e_global.py
+++ 01 - Estrutura de dados/05 - Funções/07_escopo_local_e_global.py
@@ -2,9 +2,9 @@ salario = 2000
 
 
 def salario_bonus(bonus):
-    global salario
-    salario += bonus
-    return salario
+    #global salario
+    novo_salario = salario + bonus
+    return novo_salario
 
 
-salario_bonus(500)  # 2500
+print(salario_bonus(500))  # 2500
